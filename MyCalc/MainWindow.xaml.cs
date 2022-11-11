aa1504fccb535cdc33eb9ea1aa7373897ae03693
nerooon123 <keleron123@gmail.com>
2022-11-11 20:34:20 +0500

    Добавьте файлы проекта.

---
+++ MyCalc/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCalc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string leftop = ""; // Левый операнд
+        string operation = ""; // Знак операции
+        string rightop = ""; // Правый операнд
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach(UIElement c in RootGrid.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текст кнопки
+            string s = (string)((Button)e.OriginalSource).Content;
+            // Добавляем его в текстовое поле
+            SolutionTextBlock.Text += s;
+            int num;
+            // Пытаемся преобразовать его в число
+            bool result = Int32.TryParse(s, out num);
+            // Если текст - это число
+            if (result == true)
+            {
+                // Если операция не задана
+                if (operation == "")
+                {
+                    // Добавляем к левому операнду
+                    leftop += s;
+                }
+                else
+                {
+                    // Иначе к правому операнду
+                    rightop += s;
+                }
+            }
+            // Если было введено не число
+            else
+            {
+                // Если равно, то выводим результат операции
+                if (s == "=")
+                {
+                    Update_RightOp();
+                    SolutionTextBlock.Text += rightop;
+                    operation = "";
+                }
+                // Очищаем поле и переменные
+                else if (s == "CLEAR")
+                {
+                    leftop = "";
+                    rightop = "";
+                    operation = "";
+                    SolutionTextBlock.Text = "";
+                }
+                // Получаем операцию
+                else
+                {
+                    // Если правый операнд уже имеется, то присваиваем его значение левому
+                    // операнду, а правый операнд очищаем
+                    if (rightop != "")
+                    {
+                        Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+        // Обновляем значение правого операнда
+        private void Update_RightOp()
+        {
+            try
+            {
+                int num1 = Int32.Parse(leftop);
+                int num2 = Int32.Parse(rightop);
+                // И выполняем операцию
+                switch (operation)
+                {
+                    case "+":
+                        rightop = (num1 + num2).ToString();
+                        break;
+                    case "-":
+                        rightop = (num1 - num2).ToString();
+                        break;
+                    case "*":
+                        rightop = (num1 * num2).ToString();
+                        break;
+                    case "/":
+                        rightop = (num1 / num2).ToString();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                if (e is ArgumentOutOfRangeException)
+                {
+                    MessageBox.Show("Аргумент находится вне зоны действия");
+                }
+                else if (e is FormatException)
+                {
+                    MessageBox.Show("Введен не верный формат");
+                }
+                else if (e is OverflowException)
+                {
+                    MessageBox.Show("Переполнение буфера");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            
+        }
+
+    }
+}
